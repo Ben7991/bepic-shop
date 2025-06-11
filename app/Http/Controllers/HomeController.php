c90cc2561c1fa088dcb64d4c3118a5a5a91c18d5
Bernard Teye <datssosh@gmail.com>
2025-06-11 14:53:03 +0000

    add index and login route handlers in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,18 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    //
+    public function index(): View {
+        return view('home.index');
+    }
+
+    public function login(): View {
+        return view('home.login');
+    }
 }
