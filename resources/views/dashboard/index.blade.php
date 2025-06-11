ef95d58080f74ba2972a150392d45b2c51e06a06
Bernard Teye <datssosh@gmail.com>
2025-06-11 23:20:55 +0000

    add DashboardController, index route handler as well as dashboard index.blade file

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,4 @@
+<x-layouts.dashboard>
+    <x-slot name="title">Dashboard</x-slot>
+    <h1>Testing</h1>
+</x-layouts.dashboard>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/login', [HomeController::class, 'login'])->name('login');
 Route::post('/login', [HomeController::class, 'sign_in']);
+
+Route::prefix("dashboard")->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
+});
