2b94f81cd0982c97db9439043bd32b58ffa62f7b
Bernard Teye <datssosh@gmail.com>
2025-06-11 15:20:48 +0000

    add sanitize to Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected function sanitize($data): string {
+        $data = trim($data);
+        $data = htmlspecialchars($data);
+        return stripslashes($data);
+    }
 }
