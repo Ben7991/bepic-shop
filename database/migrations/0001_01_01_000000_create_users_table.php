a9cd94e462f2e2a6fe6ffa8552dd268b1ce8c57a
Bernard Teye <datssosh@gmail.com>
2025-06-11 14:52:18 +0000

    change user table columns and user fillable property

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->string("id", 15)->primary();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username')->unique();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->enum("status", ["ACTIVE", "SUSPENDED"])->default("ACTIVE");
+            $table->enum("role", ["ADMIN", "DISTRIBUTOR"]);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
