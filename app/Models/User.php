7dc1cfad615d8187aa8d14515167139a2c451b29
Bernard Teye <datssosh@gmail.com>
2025-06-13 06:38:12 +0000

    remove unused properties in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,7 +38,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -47,7 +46,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
 
