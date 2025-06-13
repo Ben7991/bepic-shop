8a15cfaf166c2b0cdd2902a725fb6ffd0af0c8dc
Bernard Teye <datssosh@gmail.com>
2025-06-13 19:06:28 +0000

    add class to content-wrapper component in dashboard layout file

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -20,7 +20,7 @@
         <x-organisms.side-drawer :title="$title" />
         <section class="w-full lg:basis-[calc(100%-300px)] bg-white">
             <x-organisms.page-header />
-            <x-atoms.content-wrapper>
+            <x-atoms.content-wrapper class="lg:mt-5">
                 {{ $slot }}
             </x-atoms.content-wrapper>
         </section>
