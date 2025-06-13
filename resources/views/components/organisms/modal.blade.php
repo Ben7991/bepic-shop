770fefdada1dcedb14b98eb82111593aad1ffd47
Bernard Teye <datssosh@gmail.com>
2025-06-13 06:49:27 +0000

    add modal component and logout logic to page-header component to display modal

---
+++ resources/views/components/organisms/modal.blade.php
@@ -0,0 +1,38 @@
+@props(['title'])
+
+<x-atoms.backdrop id="modal-backdrop" />
+<dialog class="bg-white fixed top-1/2 left-1/2 -translate-1/2 w-[90%] md:w-[460px] rounded-lg p-4 lg:px-8 lg:py-6"
+    id="modal">
+    <header class="flex items-center justify-between mb-4">
+        <h3 class="text-2xl">{{ $title }}</h3>
+        <button class="inline-block hover:text-[var(--error-100)] cursor-pointer" id="modal-close-btn">
+            <i class="bi bi-x-lg text-xl"></i>
+        </button>
+    </header>
+    <section>{{ $slot }}</section>
+</dialog>
+
+<script>
+    const modal = document.querySelector('#modal');
+    const closeBtnModal = document.querySelector('#modal-close-btn');
+
+    closeBtnModal.addEventListener('click', function() {
+        if (modal.open) {
+            modal.open = false;
+        } else {
+            modal.open = true;
+        }
+
+        document.querySelector('#modal-backdrop').classList.replace('backdrop--show', 'backdrop--hide');
+    });
+
+    document.querySelector('#modal-backdrop').addEventListener('click', function() {
+        if (modal.open) {
+            modal.open = false;
+        } else {
+            modal.open = true;
+        }
+
+        document.querySelector('#modal-backdrop').classList.replace('backdrop--show', 'backdrop--hide');
+    });
+</script>
--- resources/views/components/organisms/page-header.blade.php
+++ resources/views/components/organisms/page-header.blade.php
@@ -5,43 +5,44 @@
             <button class="lg:hidden block" title="Show drawer menu" id="mobile-menu">
                 <i class="bi bi-list text-2xl"></i>
             </button>
-            <button class="cursor-pointer hover:text-[var(--sea-blue-100)]" title="Logout">
+            <button class="cursor-pointer hover:text-[var(--sea-blue-100)]" title="Logout" id="btn-logout">
                 <i class="bi bi-box-arrow-right text-2xl"></i>
             </button>
         </div>
     </x-atoms.content-wrapper>
 </header>
+<x-organisms.modal title="Logout">
+    <p class="mb-4 text-[var(--black-300)]">
+        Sorry, your logged-in session just expired. Please login again for a new
+        session, or you can cancel to return back to the home page
+    </p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button type="submit"
+            class="cursor-pointer py-3 bg-[var(--sea-blue-100)] text-white hover:bg-[var(--sea-blue-900)] active:bg-[var(--sea-blue-500)] rounded-lg w-[150px]">
+            Logout
+        </button>
+    </form>
+</x-organisms.modal>
 
 <script>
     const backDrop = document.querySelector('#backdrop');
     const sideDrawer = document.querySelector('#side-drawer');
     const mobileMenu = document.querySelector('#mobile-menu');
+    const btnLogout = document.querySelector('#btn-logout');
+
+    btnLogout.addEventListener('click', function() {
+        document.querySelector('#modal').open = true;
+        document.querySelector('#modal-backdrop').classList.replace('backdrop--hide', 'backdrop--show')
+    });
 
     mobileMenu.addEventListener('click', function() {
-        if (sideDrawer.classList.contains('drawer--hide')) {
-            sideDrawer.classList.replace('drawer--hide', 'drawer--show');
-        } else {
-            sideDrawer.classList.replace('drawer--show', 'drawer--hide');
-        }
-
-        if (backDrop.classList.contains('backdrop--hide')) {
-            backDrop.classList.replace('backdrop--hide', 'backdrop--show');
-        } else {
-            backDrop.classList.replace('backdrop--show', 'backdrop--hide');
-        }
+        sideDrawer.classList.replace('drawer--hide', 'drawer--show');
+        backDrop.classList.replace('backdrop--hide', 'backdrop--show');
     });
 
     backDrop.addEventListener('click', function() {
-        if (sideDrawer.classList.contains('drawer--show')) {
-            sideDrawer.classList.replace('drawer--show', 'drawer--hide');
-        } else {
-            sideDrawer.classList.replace('drawer--hide', 'drawer--show');
-        }
-
-        if (backDrop.classList.contains('backdrop--show')) {
-            backDrop.classList.replace('backdrop--show', 'backdrop--hide');
-        } else {
-            backDrop.classList.replace('backdrop--hide', 'backdrop--show');
-        }
+        sideDrawer.classList.replace('drawer--show', 'drawer--hide');
+        backDrop.classList.replace('backdrop--show', 'backdrop--hide');
     });
 </script>
