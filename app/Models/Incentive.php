47784c7d52e3d895703ce01a06445435b0df4e87
Bernard Teye <datssosh@gmail.com>
2025-06-13 19:39:06 +0000

    add Incentive model and it's migration file

---
+++ app/Models/Incentive.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Incentive extends Model
+{
+    protected $fillable = [
+        'point',
+        'award',
+        'status'
+    ];
+}
