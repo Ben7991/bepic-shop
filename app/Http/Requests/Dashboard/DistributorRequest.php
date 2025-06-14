2424e4d2914fc46251ec976ba4e0ed4fa8f4284c
Bernard Teye <datssosh@gmail.com>
2025-06-14 17:42:04 +0000

    add DistributorRequest

---
+++ app/Http/Requests/Dashboard/DistributorRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Dashboard;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class DistributorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "bail|required|regex:/^[a-zA-Z ]*$/",
+            "username" => "bail|required|regex:/^[a-zA-Z0-9]*[0-9]{1}[a-zA-Z0-9]*$/",
+            "upline_id" => "bail|required|regex:/^[A-Z]{3}[0-9]{8}$/",
+            "leg" => "required",
+            "country" => "required",
+            "phone" => "required",
+            "package" => "required"
+        ];
+    }
+}
