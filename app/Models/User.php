b9b5103cd2217c229e0d5ac539bf7e586a077c55
Bernard Teye <datssosh@gmail.com>
2025-06-14 16:51:03 +0000

    refactor getNextId, add getUserById, getUpline, createViaDistributorData and relate it to upline and distributor in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public static function getNextId(Role $role): string
     {
-        $seed = 1000;
+        $seed = 10000000;
         $totalUserWithPreferredRole = User::where('role', $role)->count();
 
         $initals = match ($role) {
@@ -60,5 +60,50 @@ class User extends Authenticatable
         };
 
         return $initals . ($totalUserWithPreferredRole + $seed + 1);
+    }
+
+    public static function getUserById(string $id): self
+    {
+        $user = User::find($id);
+
+        if ($user === null) {
+            throw new \Exception("Upline does not exist");
+        }
+
+        return $user;
+    }
+
+    public function getUpline(): Upline
+    {
+        $upline = $this->upline;
+
+        if ($upline === null) {
+            $upline = Upline::create(['user_id' => $this->id]);
+        }
+
+        return $upline;
+    }
+
+    public function upline()
+    {
+        return $this->hasOne(Upline::class);
+    }
+
+    public function distributor()
+    {
+        return $this->hasOne(Distributor::class);
+    }
+
+    public static function createViaDistributorData(string $name, string $username, string $password): self
+    {
+        $role = Role::DISTRIBUTOR;
+
+        return self::create([
+            'id' => self::getNextId($role),
+            'name' => $name,
+            'username' => $username,
+            'password' => $password,
+            'role' => $role->name
+        ]);
     }
 }
