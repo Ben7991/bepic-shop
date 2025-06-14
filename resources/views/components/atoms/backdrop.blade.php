4a072a4119844087a9b39fd365a1ae4a323b50ea
Bernard Teye <datssosh@gmail.com>
2025-06-14 22:13:23 +0000

    refactor backdrop and modal blade files

--- resources/views/components/atoms/backdrop.blade.php
+++ resources/views/components/atoms/backdrop.blade.php
@@ -1,3 +1,3 @@
 @props(['id' => 'backdrop'])
 
-<div class="w-full h-screen fixed top-0 left-0 bg-black/50 backdrop--hide" id="{{ $id }}"></div>
+<div class="w-full h-screen fixed top-0 left-0 bg-black/50 backdrop--hide z-10" id="{{ $id }}"></div>
--- resources/views/components/organisms/modal.blade.php
+++ resources/views/components/organisms/modal.blade.php
@@ -1,7 +1,7 @@
 @props(['title'])
 
 <x-atoms.backdrop id="modal-backdrop" />
-<dialog class="bg-white fixed top-1/2 left-1/2 -translate-1/2 w-[90%] md:w-[460px] rounded-lg p-4 lg:px-8 lg:py-6"
+<dialog class="bg-white fixed top-1/2 left-1/2 -translate-1/2 w-[90%] md:w-[460px] rounded-lg p-4 lg:px-8 lg:py-6 z-20"
     id="modal">
     <header class="flex items-center justify-between mb-4">
         <h3 class="text-2xl">{{ $title }}</h3>
