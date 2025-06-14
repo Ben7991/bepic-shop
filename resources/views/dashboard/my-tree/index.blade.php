811951a5d92ce16a76928f621b7d4c8f7f8fd98b
Bernard Teye <datssosh@gmail.com>
2025-06-14 18:58:51 +0000

    add heading to index.blade in my-tree folder

--- resources/views/dashboard/my-tree/index.blade.php
+++ resources/views/dashboard/my-tree/index.blade.php
@@ -1,4 +1,11 @@
 <x-layouts.dashboard>
     <x-slot name="title">My Tree</x-slot>
-    <h1>My Tree</h1>
+
+    <div class="flex gap-2 flex-col md:flex-row md:items-center md:justify-between mb-4 xl:mb-7">
+        <h1 class="text-2xl font-bold">My Tree</h1>
+        <a href="/dashboard/my-tree/create" class="hover:underline text-blue-700">
+            Add Distributor <i class="bi bi-arrow-right"></i>
+        </a>
+    </div>
+
 </x-layouts.dashboard>
