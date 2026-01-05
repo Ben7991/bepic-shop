351864f0e4056b15b26b51594bcc5fe1936d2843
Bernard Teye <bteye.ext@igus.net>
2026-01-05 13:26:55 +0000

    refactor app-logo component with new logo image

--- resources/views/components/molecules/app-logo.blade.php
+++ resources/views/components/molecules/app-logo.blade.php
@@ -1,4 +1,4 @@
 <div class='flex items-center gap-2'>
-    <img src="{{ asset('assets/img/logo.png') }}" alt="Bepic Shopping logo" class="w-16" />
-    <h3 class="text-2xl">Bepic Shopping</h3>
+    <img src="{{ asset('assets/img/logo.png') }}" alt="Energy888 logo" class="w-10" />
+    <h3 class="text-2xl">Energy888</h3>
 </div>
